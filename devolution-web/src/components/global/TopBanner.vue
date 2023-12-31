0928fd668b686b3da4e09a36fabac83bee3bd63d
Guillaume POUSSEL <gpoussel@gmail.com>
2023-12-31 21:35:30 +0100

    Set-up Pinia for state management
    This sounds like easy to set-up, but may require some
    additional steps later.

--- devolution-web/src/components/global/TopBanner.vue
+++ devolution-web/src/components/global/TopBanner.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useMetricStore } from '@/stores/metric';
+
+const metricStore = useMetricStore();
+const { formattedCoins } = storeToRefs(metricStore);
+</script>
+
 <template>
   <header class="sticky top-0 z-10 flex-none w-full mx-auto bg-white border-b border-gray-200">
     <div class="flex items-center justify-between w-full px-3 py-3 mx-auto max-w-7xl lg:px-4">
@@ -14,7 +22,7 @@
       <div class="flex items-center space-x-6">
         <div class="flex w-20">
           <img src="../../assets/icons/icons8-coin-96.png" class="h-7 mr-1" />
-          <span class="self-center whitespace-nowrap">12</span>
+          <span class="self-center whitespace-nowrap">{{ formattedCoins }}</span>
         </div>
         <div class="flex w-20">
           <img src="../../assets/icons/icons8-star-96.png" class="h-7 mr-1" />
--- devolution-web/src/views/HomeView.vue
+++ devolution-web/src/views/HomeView.vue
@@ -1,8 +1,20 @@
+<script setup lang="ts">
+import { useMetricStore } from '@/stores/metric';
+
+const metricStore = useMetricStore();
+
+// TODO: This shall no longer be used
+function increaseCoins() {
+  metricStore.addCoins(Math.round(Math.random() * 100));
+}
+</script>
+
 <template>
   <div class="flex gap-8 w-full">
     <div class="w-8/12 h-full grid grid-cols-2 gap-3">
       <div
-        class="flex items-center col-span-1 bg-black rounded-md px-4 pt-2 pb-4 border border-2 border-transparent hover:border-white hover:cursor-pointer"
+        class="flex items-center col-span-1 bg-black rounded-md px-4 pt-2 pb-4 border border-2 border-transparent hover:border-white hover:cursor-pointer select-none"
+        @click="increaseCoins()"
         v-for="n in 10"
         :key="n"
       >
