b3e9355bd6c6655e393352bd9bfd33bc9e5476f6
Guillaume POUSSEL <gpoussel@gmail.com>
2023-12-31 17:08:00 +0100

    Implement linting and formatting of vue files
    This was a pain to setup and is not totally working.
    When I hit Ctrl+S on VSCode, I still do not get the
    proper formatting, but that will be enough for now.
    
    I have also moved again the .vscode folder from root to
    devolution-web folder, as it seems to cause some
    trouble in IDE.

--- devolution-web/src/components/HelloWorld.vue
+++ devolution-web/src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
-}>()
+  msg : string;
+}>();
 </script>
 
 <template>
   <div class="greetings">
-  <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg }}</h1>
   </div>
 </template>
 
