04a5bdf80192a6da32055d069ff8465a9c2d0c6f
Guillaume POUSSEL <gpoussel@gmail.com>
2023-12-31 18:07:26 +0100

    Fix some colors of top banner
    Introduction of the first color palette

--- devolution-web/tailwind.config.js
+++ devolution-web/tailwind.config.js
@@ -1,12 +1,17 @@
+import tailwindShades from 'tailwind-shades-for-custom-colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      // Inspired by https://lospec.com/palette-list/pear36
       colors: {
         dark: '#322947',
+        green: '#3ca370',
+        blue: '#4da6ff',
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindShades],
 };
