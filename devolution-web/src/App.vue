8f12d81e65a11356e81aa2e2afcfbc4a87336bac
Guillaume POUSSEL <gpoussel@gmail.com>
2023-12-31 19:39:13 +0100

    Add the welcome modal
    This modal is currently always displayed, but
    that will be fixed as soon as the state is persisted.

--- devolution-web/src/App.vue
+++ devolution-web/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import MainContent from './components/global/MainContent.vue';
 import TopBanner from './components/global/TopBanner.vue';
+import WelcomeModal from './components/modal/WelcomeModal.vue';
 </script>
 
 <template>
   <TopBanner />
-
   <MainContent />
+
+  <!-- TODO: Display the modal only on the first launch -->
+  <WelcomeModal :visible-on-load="true" />
 </template>
