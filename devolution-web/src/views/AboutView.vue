2f1c0bfdbf6c2c5e6a89cb4d98fc92e1fb1f12af
Guillaume POUSSEL <gpoussel@gmail.com>
2024-01-07 17:09:04 +0100

    Add a way to retrieve app metadata
    The version is retrieved from package.json.
    The commit hash is retrieved using Git.

--- devolution-web/src/views/AboutView.vue
+++ devolution-web/src/views/AboutView.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+const version = __APP_VERSION__;
+const commit = __APP_COMMIT__;
+</script>
+
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <h1 class="text-xl font-semibold">About</h1>
+    <p>
+      Application version: <strong>{{ version }}</strong> ({{ commit }})
+    </p>
   </div>
 </template>
--- devolution-web/vite.config.ts
+++ devolution-web/vite.config.ts
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import gitDescribe from 'git-describe';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/devolution/',
   plugins: [vue()],
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __APP_COMMIT__: JSON.stringify(gitDescribe.gitDescribeSync().hash),
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
