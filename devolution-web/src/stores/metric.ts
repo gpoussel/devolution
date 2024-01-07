8e619e38ecb0d30ed1e82258c3d1a268df3b0262
Guillaume POUSSEL <gpoussel@gmail.com>
2024-01-07 21:00:01 +0100

    Remove useless getter

--- devolution-web/src/stores/metric.ts
+++ devolution-web/src/stores/metric.ts
@@ -1,16 +1,12 @@
 import { ref, type Ref } from 'vue';
 import { defineStore } from 'pinia';
 import Decimal from 'break_infinity.js';
-import * as numberFormat from 'swarm-numberformat';
 
 export const useMetricStore = defineStore('metric', {
   state: () => {
     const coins = ref(new Decimal(0)) as Ref<Decimal>;
     const coinsPerSecond = ref(new Decimal(0)) as Ref<Decimal>;
     return { coins, coinsPerSecond };
-  },
-  getters: {
-    formattedCoinsPerSecond: (state) => numberFormat.formatShort(state.coinsPerSecond),
   },
   actions: {
     addCoins(value: Decimal) {
