64ff771673de3f5d2787559a7df33f5b61ad5b63
Guillaume POUSSEL <gpoussel@gmail.com>
2024-01-08 20:49:38 +0100

    Create an ExternalLink component
    This will reduce duplication of styles

--- devolution-web/src/views/AboutView.vue
+++ devolution-web/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import IconGithub from '@/assets/svg/icon-github.svg';
+import ExternalLink from '@/components/utils/ExternalLink.vue';
 
 const version = __APP_VERSION__;
 const commit = __APP_COMMIT__;
@@ -14,8 +15,8 @@ const commit = __APP_COMMIT__;
       <div class="w-1/2">
         <p>
           This game was developped in January 2024, for the
-          <a class="text-blue hover:text-blue-200" href="https://itch.io/jam/nyigj-2024"
-            >New Year Incremental Game Jam (NYIGJ) 2024</a
+          <ExternalLink target="https://itch.io/jam/nyigj-2024"
+            >New Year Incremental Game Jam (NYIGJ) 2024</ExternalLink
           >.
         </p>
         <p>
@@ -24,8 +25,8 @@ const commit = __APP_COMMIT__;
         <h2 class="text-lg font-semibold mt-4">Author</h2>
         <p>
           Guillaume Poussel (source code available on
-          <a class="text-blue hover:text-blue-200" href="https://github.com/gpoussel/devolution">
-            <IconGithub class="inline text-inherit w-4 -mt-2 ml-1" /> GitHub</a
+          <ExternalLink target="https://github.com/gpoussel/devolution">
+            <IconGithub class="inline text-inherit w-4 -mt-2 ml-1" /> GitHub</ExternalLink
           >)
         </p>
       </div>
@@ -33,25 +34,22 @@ const commit = __APP_COMMIT__;
         <h2 class="text-lg font-semibold">Licensing</h2>
         <p>
           Devolution is made available under the
-          <a
-            class="text-blue hover:text-blue-200"
-            href="https://github.com/gpoussel/devolution/blob/main/LICENSE"
-            >Apache License, version 2.0</a
+          <ExternalLink target="https://github.com/gpoussel/devolution/blob/main/LICENSE"
+            >Apache License, version 2.0</ExternalLink
           >. Some assets have been downloaded or purchased from
-          <a class="text-blue hover:text-blue-200" href="itch.io">itch.io</a>. Some of them have
-          been edited from their orginal form, which is allowed under their license. If you would
-          like to use their assets for your own project, then
+          <ExternalLink target="https://itch.io">itch.io</ExternalLink>. Some of them have been
+          edited from their orginal form, which is allowed under their license. If you would like to
+          use their assets for your own project, then
           <strong>PLEASE PURCHASE IT DIRECTLY FROM THEM</strong>.
         </p>
         <ul class="list-disc ml-6">
           <li>
             Status icons (coin, star, read heart and bug) are provided by
-            <a class="text-blue hover:text-blue-200" href="https://icons8.com/">Icons8</a>.
+            <ExternalLink target="https://icons8.com/">Icons8</ExternalLink>.
           </li>
           <li>
             Application icons are provided by
-            <a class="text-blue hover:text-blue-200" href="https://flowbite.com/icons/">Flowbite</a
-            >.
+            <ExternalLink target="https://flowbite.com/icons/">Flowbite</ExternalLink>.
           </li>
         </ul>
       </div>
