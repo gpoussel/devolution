c733eccde2867cb615735170ff62afe0fdb76cb4
Guillaume POUSSEL <gpoussel@gmail.com>
2024-01-08 21:07:45 +0100

    Add icons in top banner
    It will reduce used space

--- devolution-web/src/components/global/top/ApplicationLinks.vue
+++ devolution-web/src/components/global/top/ApplicationLinks.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import IconCog from '@/assets/svg/icon-cog.svg';
+import IconHome from '@/assets/svg/icon-home.svg';
+import IconInfo from '@/assets/svg/icon-info.svg';
+
 import ApplicationLink from './ApplicationLink.vue';
 </script>
 
 <template>
   <div class="flex items-center">
     <ul class="flex-col pt-6 lg:flex-row lg:self-center lg:py-0 lg:flex">
-      <ApplicationLink route="/about">About</ApplicationLink>
-      <ApplicationLink route="/settings">Settings</ApplicationLink>
+      <ApplicationLink route="/"><IconHome class="w-4 text-inherit" /></ApplicationLink>
+      <ApplicationLink route="/about"><IconInfo class="w-4 text-inherit" /></ApplicationLink>
+      <ApplicationLink route="/settings"><IconCog class="w-4 text-inherit" /></ApplicationLink>
     </ul>
   </div>
 </template>
