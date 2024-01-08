b327612262cf32b5f10e919f8f6cd1f225f389b0
Guillaume POUSSEL <gpoussel@gmail.com>
2024-01-08 10:39:22 +0100

    Remove multiplier (not used)

--- devolution-web/src/game/design/progress.ts
+++ devolution-web/src/game/design/progress.ts
@@ -6,7 +6,6 @@ export interface ProgressLevel {
     coinsPerSecond: Decimal;
     coins: Decimal;
   };
-  multiplier: Decimal;
 }
 
 export const PROGRESS_LEVELS: ProgressLevel[] = [
@@ -16,7 +15,6 @@ export const PROGRESS_LEVELS: ProgressLevel[] = [
       coins: Decimal.fromNumber(3000),
       coinsPerSecond: Decimal.fromNumber(300),
     },
-    multiplier: Decimal.fromNumber(1),
   },
   {
     name: 'Small Studio',
@@ -24,7 +22,6 @@ export const PROGRESS_LEVELS: ProgressLevel[] = [
       coins: Decimal.fromNumber(8000),
       coinsPerSecond: Decimal.fromNumber(800),
     },
-    multiplier: Decimal.fromNumber(1),
   },
   {
     name: 'Medium Studio',
@@ -32,7 +29,6 @@ export const PROGRESS_LEVELS: ProgressLevel[] = [
       coins: Decimal.fromNumber(12000),
       coinsPerSecond: Decimal.fromNumber(1200),
     },
-    multiplier: Decimal.fromNumber(1),
   },
   {
     name: 'Large Studio',
@@ -40,7 +36,6 @@ export const PROGRESS_LEVELS: ProgressLevel[] = [
       coins: Decimal.fromNumber(19000),
       coinsPerSecond: Decimal.fromNumber(1900),
     },
-    multiplier: Decimal.fromNumber(1),
   },
   {
     name: 'AAA Studio',
@@ -48,7 +43,6 @@ export const PROGRESS_LEVELS: ProgressLevel[] = [
       coins: Decimal.fromNumber(25000),
       coinsPerSecond: Decimal.fromNumber(2500),
     },
-    multiplier: Decimal.fromNumber(1),
   },
   {
     name: 'Game Industry Leader',
@@ -56,6 +50,5 @@ export const PROGRESS_LEVELS: ProgressLevel[] = [
       coins: Decimal.fromNumber(50000),
       coinsPerSecond: Decimal.fromNumber(4000),
     },
-    multiplier: Decimal.fromNumber(1),
   },
 ];
