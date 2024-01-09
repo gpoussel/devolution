569068fc01602117dda2855d3fbe40a13fa55736
Guillaume POUSSEL <gpoussel@gmail.com>
2024-01-09 22:21:21 +0100

    Update colors

--- devolution-web/src/components/modal/ClickActionModal.vue
+++ devolution-web/src/components/modal/ClickActionModal.vue
@@ -113,7 +113,7 @@ onUnmounted(() => {
             <div class="bg-green p-4 sm:flex sm:flex-row-reverse">
               <button
                 type="button"
-                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
                 :disabled="!completed"
                 :class="{
                   'hover:bg-gray-300': completed,
--- devolution-web/src/components/modal/SettingsProgressModal.vue
+++ devolution-web/src/components/modal/SettingsProgressModal.vue
@@ -101,7 +101,7 @@ function done() {
             <div class="bg-green p-4 sm:flex sm:flex-row-reverse">
               <button
                 type="button"
-                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto hover:bg-gray-300"
+                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto hover:bg-gray-300"
                 @click="done"
               >
                 {{ buttonText }}
